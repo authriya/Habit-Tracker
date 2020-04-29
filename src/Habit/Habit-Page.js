510f6e35d3686054d521e6229b3f0c583a39b21f
Supriya Ganesh <supriya@thinkful.com>
2020-04-29 16:06:24 -0400

    habit page routers

--- src/Habit/Habit-Page.js
+++ src/Habit/Habit-Page.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ApiContext from '../ApiContext';
 import {Link} from 'react-router-dom';
-import {findHabit} from '../Habits-Helpers';
 import './Habit-Page.css'
+import HabitsApiService from '../Services/habits-api-service';
 
 class HabitPage extends React.Component {
+    state = {
+        name: '',
+        description: ''
+    }
     static defaultProps = {
         match: {
             params: {}
@@ -12,26 +16,43 @@ class HabitPage extends React.Component {
     }
     static contextType = ApiContext
 
+    componentDidMount() {
+        const {habitId} = this.props.match.params
+        const habitIdInteger = parseInt(habitId) 
+        HabitsApiService.getHabitById(habitIdInteger)
+            .then(habit => {
+                this.setState({
+                    name: habit.name,
+                    description: habit.description
+                })
+            })
+            .catch((error) => {
+                console.error({error})
+            })
+    }
+
     handleDeleteHabit = e => {
         e.preventDefault()
         const {habitId} = this.props.match.params
         const habitIdInteger = parseInt(habitId)
-        this.context.deleteHabit(habitIdInteger)
-        this.props.history.push('/overview')
+        HabitsApiService.deleteAHabit(habitIdInteger)
+            .then(() => {
+                this.context.deleteHabit(habitIdInteger)
+            })
+            .catch((error) => {
+                console.error({error})
+            })
+            .then(this.props.history.push('/overview'))
     }
+
     render() {
-        const {habits = []} = this.context
+        const {name, description} = this.state
         const {habitId} = this.props.match.params
-        const habit = findHabit(habits, parseInt(habitId))
-        console.log(habit)
-        if(!habit) {
-            return null
-        }
         return(
             <div className="Habit_Page">
-                <h2 className="habit_title">{habit.name}</h2>
-                <p className="habit_description">{habit.description}</p>
-                <Link to ={`/edit-habit/${habit.id}`}><input type="button" value="Edit Habit" className="habit_page_button"/></Link><input type="button" value="Delete Habit" className="habit_page_button" onClick = {this.handleDeleteHabit}/>
+                <h2 className="habit_title">{name}</h2>
+                <p className="habit_description">{description}</p>
+                <Link to ={`/edit-habit/${habitId}`}><input type="button" value="Edit Habit" className="habit_page_button"/></Link><input type="button" value="Delete Habit" className="habit_page_button" onClick = {this.handleDeleteHabit}/>
             </div>
         )
     }
